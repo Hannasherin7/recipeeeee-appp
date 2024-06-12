1f0ae0991347241dfd0e880f9742cc628fe08287
hannasherin <hannasherin7110@gmail.com>
2024-06-13 00:09:50 +0530

    deleterecipe

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Add } from './components/Add';
 import { View } from './components/View';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Search } from './components/Search';
+import { Delete } from './components/Delete';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
     <Routes>
       <Route path='/'element={(<Add/>)}/>
       <Route path='/search'element={(<Search/>)}/>
+      <Route path='/delete'element={(<Delete/>)}/>
       <Route path='/view'element={(<View/>)}/>
     </Routes>
     </BrowserRouter>
